af0d9e8a47f71b47ab20cdfc2fcce80f1ae300e5
DIMEBIS123123 <villlishely@gmail.com>
2026-04-09 10:24:30 +0300

    Редакция README

---
+++ src/components/SettingBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SettingBar = () => {
+	return <div className='bar'></div>
+}
+
+export default SettingBar
---
+++ src/components/ToolBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/toolbar.scss'
+
+const Toolbar = () => {
+	return <div className='bar'></div>
+}
+
+export default Toolbar
